421d2ef8cbfd641b14ed4a8faeb41bd557970a23
n0dustoll3ns <fominykh.dg@gmail.com>
2022-05-29 00:31:36 +0700

    complete list of air companies

--- src/elements/sidebar/components/CompanyFilter/CompanyFilter.tsx
+++ src/elements/sidebar/components/CompanyFilter/CompanyFilter.tsx
@@ -1,28 +1,29 @@
 import React from 'react'
-
 import s from '../../SideBar.module.scss';
-
-
+import { Result, FlightElement, FlightFlight, Leg, Segment, AirlineAlliance, Caption } from '../../../../models/ApiInterface'
+import data from '../../../../models/flights.json'
 interface Props { }
 
 export const CompanyFilter = (props: Props) => {
+
+  let res: FlightElement = JSON.parse(JSON.stringify(data));
+  let airlines: string[] = [];
+
+  Object.values(Caption).forEach(company => airlines.push(company))
   return (
     <div className={s.filter_container}>
       <div className="title">
         <h5>Авиакомпании</h5>
         <form>
-          <div className="checkbox">
-            <label>
-              <input type="checkbox" value="ascending_price" checked={true} />
-              LOT
-            </label>
-          </div>
-          <div className="checkbox">
-            <label>
-              <input type="checkbox" value="ascending_price" />
-              Aeroflot
-            </label>
-          </div>
+          {airlines.map((company: string) => (
+            <div className="checkbox">
+              <label>
+                <input type="checkbox" value={company} checked={true} />
+                {company}
+              </label>
+            </div>
+          ))
+          }
         </form>
       </div>
     </div>
